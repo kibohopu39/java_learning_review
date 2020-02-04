a2e3586f988bf28329e524dac117abda0261ba9e
kibohopu39 <changewiththetime@gmail.com>
2020-02-04 15:50:06 +0800

    Add annotation

--- Java_classes/src/basic_review/Kai_java_01.java
+++ Java_classes/src/basic_review/Kai_java_01.java
@@ -1,10 +1,21 @@
 package basic_review;
 
-public class Kai_java_01 {
-// 
-	public static void main(String[] args) {
-		
+import javax.swing.JOptionPane;
 
+public class Kai_java_01 {
+// create a panel for asking using
+	
+	public static void main(String[] args) {
+		//import JOptionPane, and use its static methods
+		//showInputDialog is a method of typing your content on your window
+		String x = JOptionPane.showInputDialog("Input a number");
+		String y = JOptionPane.showInputDialog("Input a number");
+		
+		//transform type
+		int intX = Integer.parseInt(x);
+		int intY = Integer.parseInt(y);
+		int intR = intX + intY;
+		JOptionPane.showMessageDialog(null, intX + "+" + intY+"=" +intR);
 	}
 
 }
